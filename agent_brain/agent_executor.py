dff8a4ad5420d3b5333584e811b7032af1dc9e53
Shreyas-V06 <shreyasvakkiyel@gmail.com>
2025-04-18 19:20:56 +0530

    Added agent executor

---
+++ agent_brain/agent_executor.py
@@ -0,0 +1,17 @@
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+from tools.todo_tools import create_todo_agent,update_todo_agent,delete_todo_agent,get_all_todos_agent
+
+
+def initialize_agent_executor(AgentLLM):
+    prompt = hub.pull("hwchase17/react")
+    tools=[create_todo_agent,update_todo_agent,delete_todo_agent,get_all_todos_agent]
+    agent = create_react_agent(AgentLLM,tools,prompt)
+    agent_executor = AgentExecutor(agent=agent,tools=tools,verbose=True)
+    return agent_executor
+
+
+def invoke_agent(user_input:str):
+    agent_executor=initialize_agent_executor()
+    agent_executor.invoke({"input":user_input})
+
